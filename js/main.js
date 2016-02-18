95a6afda5f6b1178315915afcb0bb3a422548fb4
Ale Casco <alecasco.14@gmail.com>
2016-02-18 18:25:25 -0300

    Primera version - falta carousel

---
+++ js/main.js
@@ -0,0 +1,5 @@
+//var carousel = new ch.Carousel(".myCarousel", {"pagination": true});
+
+var carousel = $(".myCarousel").carousel({"pagination": true});
+
+//var tabs = $(".YOUR_SELECTOR_Tabs").tabs();
