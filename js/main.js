ed6f9ec26edfd4634005e6f037db30e1be78b002
Ale Casco <alecasco.14@gmail.com>
2016-02-18 21:26:49 -0300

    Corrección de chico js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,1 @@
-//var carousel = new ch.Carousel(".myCarousel", {"pagination": true});
 
-var carousel = $(".myCarousel").carousel({"pagination": true});
-
-//var tabs = $(".YOUR_SELECTOR_Tabs").tabs();
